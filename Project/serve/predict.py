e231ab35b85adeb47f498dcc08c69c0cb1317dd8
EC2 Default User <ec2-user@ip-172-16-79-202.us-west-2.compute.internal>
2019-10-13 23:08:13 +0000

    process data, implement, train, deploy, test model

--- Project/serve/predict.py
+++ Project/serve/predict.py
@@ -65,13 +65,12 @@ def predict_fn(input_data, model):
     if model.word_dict is None:
         raise Exception('Model has not been loaded properly, no word_dict.')
     
-    # TODO: Process input_data so that it is ready to be sent to our model.
+    # TODO: DONE: Process input_data so that it is ready to be sent to our model.
     #       You should produce two variables:
     #         data_X   - A sequence of length 500 which represents the converted review
     #         data_len - The length of the review
 
-    data_X = None
-    data_len = None
+    data_X, data_len = convert_and_pad(model.word_dict, review_to_words(input_data))
 
     # Using data_X and data_len we construct an appropriate input tensor. Remember
     # that our model expects input data of the form 'len, review[500]'.
@@ -84,9 +83,11 @@ def predict_fn(input_data, model):
     # Make sure to put the model into evaluation mode
     model.eval()
 
-    # TODO: Compute the result of applying the model to the input data. The variable `result` should
-    #       be a numpy array which contains a single integer which is either 1 or 0
+    # TODO: DONE: Compute the result of applying the model to the input data. The variable `result` should
+    #       be a numpy array which contains a single integer which is either 1 or 0    
+    with torch.no_grad():
+        prediction = model.forward(data)
 
-    result = None
-
+    result = np.round(prediction.numpy())
+    
     return result
--- Project/train/train.py
+++ Project/train/train.py
@@ -54,7 +54,6 @@ def _get_train_data_loader(batch_size, training_dir):
 
     return torch.utils.data.DataLoader(train_ds, batch_size=batch_size)
 
-
 def train(model, train_loader, epochs, optimizer, loss_fn, device):
     """
     This is the training method that is called by the PyTorch training script. The parameters
@@ -66,12 +65,27 @@ def train(model, train_loader, epochs, optimizer, loss_fn, device):
     loss_fn      - The loss function used for training.
     device       - Where the model and data should be loaded (gpu or cpu).
     """
-    
-    # TODO: Paste the train() method developed in the notebook here.
-
-    pass
-
-
+    # TODO: DONE: Paste the train() method developed in the notebook here.
+    for epoch in range(1, epochs + 1):
+        model.train()
+        total_loss = 0
+        for batch in train_loader:
+            batch_X, batch_y = batch
+            
+            batch_X = batch_X.to(device)
+            batch_y = batch_y.to(device)
+            
+            # TODO: DONE: Complete this train method to train the model provided.
+            optimizer.zero_grad()
+            
+            logps = model(batch_X)
+            loss = loss_fn(logps, batch_y)
+            loss.backward()
+            optimizer.step()
+            
+            total_loss += loss.data.item()
+        print("Epoch: {}, BCELoss: {}".format(epoch, total_loss / len(train_loader)))
+                               
 if __name__ == '__main__':
     # All of the model parameters and training parameters are sent as arguments when the script
     # is executed. Here we set up an argument parser to easily access the parameters.
